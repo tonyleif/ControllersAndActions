241012e88243477c9e0e0323cc6a0bc6d917865e
Anthony Leif <tonyleif@hotmail.com>
2017-10-31 21:40:49 -0500

    Page 449

--- ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -11,7 +11,7 @@ namespace ControllersAndActions.Controllers
         // GET: Example
         public ActionResult Index()
         {
-            return View("HomePage");
+            return View();
         }
     }
 }
