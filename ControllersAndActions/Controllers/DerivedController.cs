1412bb23cbd3df84f399deaff712cf9a3e937671
Anthony Leif <tonyleif@hotmail.com>
2017-10-31 21:01:36 -0500

    Page 442

---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+
+        public void ProduceOutput()
+        {
+            if (Server.MachineName == "TINY")
+            {
+                Response.Redirect("/Basic/Index");
+            }
+            else
+            {
+                Response.Write("Controller: Derived, Action: ProduceOutput");
+            }
+        }
+    }
+}
