00abba814eda9df17656fc92e4847d395654ddfe
Anthony Leif <tonyleif@hotmail.com>
2017-11-01 22:27:05 -0500

    Page 459

--- ControllersAndActions.Tests/ActionTests.cs
+++ ControllersAndActions.Tests/ActionTests.cs
@@ -13,10 +13,32 @@ namespace ControllersAndActions.Tests
             //Arrange
             ExampleController target = new ExampleController();
             //Act
-            ViewResult result = (ViewResult)target.Index();
+            //ViewResult result = (ViewResult)target.Index();
+            RedirectToRouteResult result = target.Redirect();
             //Assert
-            Assert.AreEqual("", result.ViewName);
+            //Assert.AreEqual("", result.ViewName);
+
+            //Assert.AreEqual("Hello", result.ViewBag.Message);
+
+            //Assert.IsTrue(result.Permanent);
+            //Assert.AreEqual("/Example/Index", result.Url);
+
+            Assert.IsFalse(result.Permanent);
+            Assert.AreEqual("Example", result.RouteValues["controller"]);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("MyID", result.RouteValues["ID"]);
         }
+
+        [TestMethod]
+        public void ControllerStatusCodeTest()
+        {
+            // Arrange - create the controller
+            ExampleController target = new ExampleController();
+            // Act - call the action method
+            HttpStatusCodeResult result = target.StatusCode();
+            // Assert - check the result
+            Assert.AreEqual(401, result.StatusCode);
+        }.
 
         [TestMethod]
         public void ViewSelectionTest()
--- ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -8,10 +8,35 @@ namespace ControllersAndActions.Controllers
 {
     public class ExampleController : Controller
     {
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            DateTime date = DateTime.Now;
-            return View(date);
+            ViewBag.Message = "Hello";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        //public ActionResult Index()
+        //{
+        //    DateTime date = DateTime.Now;
+        //    return View(date);
+        //}
+
+        public RedirectToRouteResult Redirect()
+        {
+            return RedirectToRoute(new { controller = "Example", action = "Index", ID = "MyID" });
+        }
+
+        //public RedirectResult Redirect()
+        //{
+        //    return Redirect("/Example/Index");
+        //    //return RedirectPermanent("/Example/Index");
+        //}
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            //return new HttpStatusCodeResult(404, "URL cannot be serviced");
+            //return HttpNotFound();
+            return new HttpUnauthorizedResult();
         }
     }
 }
